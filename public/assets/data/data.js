e5a3357f6e0b62925e195fc874bfdf817e092e9c
Renegade Confidence <confidencenyirenda@Renegades-MacBook-Air.local>
2025-09-06 13:22:46 +0200

    fixed netlify bug

--- public/assets/data/data.js
+++ public/assets/data/data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `/public/assets/images/troll.jpg`,
+        profilePic: `/assets/images/troll.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -13,19 +13,19 @@ export const tweetsData = [
     },    
     {
         handle: `@Elon ✅`,
-        profilePic: `/public/assets/images/musk.png`,
+        profilePic: `/assets/images/musk.png`,
         likes: 6500,
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
                   {
                 handle: `@TomCruise ✅`,
-                profilePic: `/public/assets/images/tcruise.png`,
+                profilePic: `/assets/images/tcruise.png`,
                 tweetText: `Yes! Sign me up! 😎🛩`,
             },
                   {
                 handle: `@ChuckNorris ✅`,
-                profilePic: `/public/assets/images/chucknorris.jpeg`,
+                profilePic: `/assets/images/chucknorris.jpeg`,
                 tweetText: `I went last year😴`,
             },
         ],
@@ -35,19 +35,19 @@ export const tweetsData = [
     },
         {
         handle: `@NoobCoder12`,
-        profilePic: `/public/assets/images/flower.png`,
+        profilePic: `/assets/images/flower.png`,
         likes: 10,
         retweets: 3,
         tweetText: `Are you a coder if you only know HTML?`,
         replies: [
             {
                 handle: `@StackOverflower ☣️`,
-                profilePic: `/public/assets/images/overflow.png`,
+                profilePic: `/assets/images/overflow.png`,
                 tweetText: `No. Onviosuly not. Go get a job in McDonald's.`,
             },
             {
                 handle: `@YummyCoder64`,
-                profilePic: `/public/assets/images/love.png`,
+                profilePic: `/assets/images/love.png`,
                 tweetText: `You are wonderful just as you are! ❤️`,
             },
         ],
